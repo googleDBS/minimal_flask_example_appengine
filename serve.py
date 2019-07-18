24d0040825e9cc5d76211cb1ad74355a090aa86a
majickdave <majickdave@gmail.com>
2019-07-17 18:03:11 -0700

    print predicted

--- serve.py
+++ serve.py
@@ -1,10 +1,29 @@
 from fastai.vision import *
+defaults.device = torch.device('cpu')
 
-def load_model(folder):
+def load_model():
     """ Load model from path """
-    path_to_model = Path(folder)
+    path_to_model = Path('models')
 
     # Load learner
     learn = load_learner(path_to_model)
 
     return learn
+
+def load_image():
+    # Location of bucket with image
+    path = Path('images')
+    url = path/'bear.jpg'
+
+    # Download Image
+    img = open_image(url)
+
+    return img
+def predict_image(img):
+    learn = load_model()
+    # Return Prediction
+    pred_class,pred_idx,outputs = learn.predict(img)
+
+    return pred_class
+
+print(predict_image(load_image()))
